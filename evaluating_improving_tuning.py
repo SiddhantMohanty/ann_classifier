ab41abfd42952092b4ac3a8d72c12ec1c891db24
Siddhant Mohanty <53761250+SiddhantMohanty@users.noreply.github.com>
2019-10-02 08:56:38 +0530

    Update evaluating_improving_tuning.py
    ANN with parameter tuning.

--- evaluating_improving_tuning.py
+++ evaluating_improving_tuning.py
@@ -1,27 +1,18 @@
-# Artificial Neural Network
+# Artificial Neural Network with parameter tuning
 
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+#Part 1 - Data Preprocessing
 
-# Installing Tensorflow
-# pip install tensorflow
-
-# Installing Keras
-# pip install --upgrade keras
-
-# Part 1 - Data Preprocessing
-
-# Importing the libraries
+#Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+#Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
-# Encoding categorical data
+#Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
@@ -31,51 +22,51 @@ onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
 
-# Splitting the dataset into the Training set and Test set
+#Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
+#Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Part 2 - Now let's make the ANN!
+#Part 2 - Building the ANN!
 
-# Importing the Keras libraries and packages
+#Importing the Keras libraries and packages
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 
-# Initialising the ANN
+#Initialising the ANN
 classifier = Sequential()
 
-# Adding the input layer and the first hidden layer
+#Adding the input layer and the first hidden layer
 classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
 # classifier.add(Dropout(p = 0.1))
 
-# Adding the second hidden layer
+#Adding the second hidden layer
 classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
 # classifier.add(Dropout(p = 0.1))
 
-# Adding the output layer
+#Adding the output layer
 classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
 
-# Compiling the ANN
+#Compiling the ANN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-# Fitting the ANN to the Training set
+#Fitting the ANN to the Training set
 classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
 
-# Part 3 - Making predictions and evaluating the model
+#Part 3 - Making predictions and evaluating the model
 
-# Predicting the Test set results
+#Predicting the Test set results
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5)
 
-# Predicting a single new observation
+#Predicting a single new observation
 """Predict if the customer with the following informations will leave the bank:
 Geography: France
 Credit Score: 600
@@ -90,13 +81,13 @@ Estimated Salary: 50000"""
 new_prediction = classifier.predict(sc.transform(np.array([[0.0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])))
 new_prediction = (new_prediction > 0.5)
 
-# Making the Confusion Matrix
+#Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-# Part 4 - Evaluating, Improving and Tuning the ANN
+#Part 4 - Evaluating, Improving and Tuning the ANN
 
-# Evaluating the ANN
+#Evaluating the ANN
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 from keras.models import Sequential
@@ -113,10 +104,10 @@ accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, c
 mean = accuracies.mean()
 variance = accuracies.std()
 
-# Improving the ANN
-# Dropout Regularization to reduce overfitting if needed
+#Improving the ANN
+#Dropout Regularization to reduce overfitting if needed
 
-# Tuning the ANN
+#Tuning the ANN
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import GridSearchCV
 from keras.models import Sequential
